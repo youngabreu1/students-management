8e4a806dd8c8c9ea78d0f97ece84cc4c400307ad
Gabriel Carlos <gabrielcarlos0705@gmail.com>
2024-06-20 19:57:57 -0400

    feat: routes configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { StudentsDetailsComponent } from './components/students-details/students-details.component';
+import { StudentsListComponent } from './components/students-list/students-list.component';
+import { StudentFormComponent } from './components/student-form/student-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'students', component: StudentsListComponent },
+    { path: 'students/create', component: StudentFormComponent },
+    { path: 'students/edit/:id', component: StudentFormComponent },
+    { path: 'students/details/:id', component: StudentsDetailsComponent },
+    { path: 'details', component: StudentsDetailsComponent }
+];
