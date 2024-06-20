5595f8750517bb3718e323dacec9de89047c0db3
Gabriel Carlos <gabrielcarlos0705@gmail.com>
2024-06-20 19:57:16 -0400

    feat: students-details component

---
+++ src/app/components/students-details/students-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-students-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './students-details.component.html',
+  styleUrl: './students-details.component.css'
+})
+export class StudentsDetailsComponent {
+
+}
