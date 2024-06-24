f40734f8c8d44f6abb6060afa857577be620c117
Gabriel Carlos <gabrielcarlos0705@gmail.com>
2024-06-24 18:20:24 -0400

    feat: student model and service

---
+++ src/app/services/student.service.ts
@@ -0,0 +1,34 @@
+// src/app/student.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Student } from '../models/student';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StudentService {
+  private apiUrl = 'http://localhost:3000/students';
+
+  constructor(private http: HttpClient) { }
+
+  getStudents(): Observable<Student[]> {
+    return this.http.get<Student[]>(this.apiUrl);
+  }
+
+  getStudent(id: number): Observable<Student> {
+    return this.http.get<Student>(`${this.apiUrl}/${id}`);
+  }
+
+  createStudent(student: Student): Observable<Student> {
+    return this.http.post<Student>(this.apiUrl, student);
+  }
+
+  updateStudent(student: Student): Observable<Student> {
+    return this.http.put<Student>(`${this.apiUrl}/${student.id}`, student);
+  }
+
+  deleteStudent(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
