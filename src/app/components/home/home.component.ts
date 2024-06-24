3b5f3c416b50ed21704aa044b7f560b0be040f08
Gabriel Carlos <gabrielcarlos0705@gmail.com>
2024-06-24 18:20:43 -0400

    feat: http requests

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CaroulselComponent } from '../caroulsel/caroulsel.component';
+import { StudentService } from '../../services/student.service';
+import { Student } from '../../models/student';
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -7,7 +9,17 @@ import { CaroulselComponent } from '../caroulsel/caroulsel.component';
     CaroulselComponent
   ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  students: Student[] = new Array<Student>();
+
+  constructor(private studentService: StudentService) { }
+
+  ngOnInit(): void {
+    this.studentService.getStudents().subscribe((students: Student[]) => {
+      this.students = students;
+      console.log(this.students)
+    });
+  }
 }
