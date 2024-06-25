1c4e6e15944b29b9673cbe8acf6a7ef1dc9a7f8b
Gabriel Carlos <gabrielcarlos0705@gmail.com>
2024-06-25 09:37:22 -0400

    minor changes

--- src/app/models/student.ts
+++ src/app/models/student.ts
@@ -1,6 +1,11 @@
 export class Student {
   id?: number;
-  name?: string;
-  age?: number;
-  major?: string;
+  name?: string
+  registration?: number
+  course?: string
+  campus?: string
+  address?: string
+  birthdate?: string
+  phone?: string
+  cpf?: string
 }
